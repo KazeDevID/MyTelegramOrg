538e8311e4722431273ed50ae7924bdfb78a855c
Kaze <Michaelagam91@gmail.com>
2022-04-13 07:17:12 +0800

    MyTelegramOrg

---
+++ README.md
@@ -0,0 +1,23 @@
+### My Telegram Org Bot
+
+
+## Deploy to Heroku👇
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FPunyaChael%2FMyTelegramOrg&template=https%3A%2F%2Fgithub.com%2FPunyaChael%2FMyTelegramOrg)
+
+
+</a>
+    <a href="https://telegram.me/NothingSupportBot">
+        <img
+            src="https://img.shields.io/badge/Support Group-blue?&style=for-the-badge&logo=Telegram"
+            alt="Telegram"
+        >
+
+
+
+</a>
+    <a href="https://telegram.me/ItsMeKaze91">
+        <img
+            src="https://img.shields.io/badge/Updates Channel-blue?&style=for-the-badge&logo=Telegram"
+            alt="Telegram"
+        >
---
+++ translation.py
@@ -0,0 +1,25 @@
+class Translation(object):
+    START_TEXT = """Hi!
+Hai Saya Bot My.telegram.org Sederhana.
+
+Membantu Anda Untuk Mendapatkan API ID & API HASH Masukkan Nomor Telepon Telegram Anda Dengan Kode Negara.
+
+Join To 
+Group Update: @NothingSupportBot
+Group Anime: @AniLoversIndo
+Channel Support: @ItsMeKaze91
+
+
+Klik /start Untuk Memulai Bot nya"""
+    AFTER_RECVD_CODE_TEXT = """I see!
+Sekarang silakan kirim kode Telegram yang Anda terima dari Telegram!
+
+kode ini hanya digunakan untuk tujuan mendapatkan ID APP dari my.telegram.org
+\njika Anda tidak mempercayai pengembang bot ini, harap kunjungan web terpercaya telegram my.telegram.org
+
+Klik /start Untuk Memulai Ulang Botnya"""
+    BEFORE_SUCC_LOGIN = "Kode Diterima. Scarpping Web Page..."
+    ERRED_PAGE = "Gagal Mendapatkan Api Id\nSilakan Hubungi Kami Di @KenalSayaaa"
+    CANCELLED_MESG = "Bye! Silahkan /start kembali untuk mengulang Bot"
+    IN_VALID_CODE_PVDED = "Kode OTP yang anda Masukan SALAH!! Harap Masukan Kode OTP dengan benar!!!"
+    IN_VALID_PHNO_PVDED = "No HP yang anda masukan SALAH, Silahkan Masukkan Nomor Telepon Telegram Anda Dengan Format Kode Negara.\nContoh: +628xxxxxxx"
