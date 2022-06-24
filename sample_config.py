af37a9444b2c7b445cd6beaa6d0bba1014e0f7c3
Kaze <101078139+KazeDevID@users.noreply.github.com>
2022-06-24 20:07:15 +0800

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,9 +12,9 @@ class Config:
     CHUNK_SIZE = 10280
     # MyTelegram.org
     # configurtion required while creating new application
-    APP_TITLE = os.environ.get("APP_TITLE", "motechgroup")
-    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "Bx_Botz")
-    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/MufazTG")
+    APP_TITLE = os.environ.get("APP_TITLE", "scarpbot")
+    APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "scarpbot")
+    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/KenalSayaaa")
     # these platform informations were obtained
     # on 27.01.2020 21:15:50 IST
     APP_PLATFORM = [
@@ -29,7 +29,7 @@ class Config:
     ]
     # if any of the platform, does not work
     # please reopen
-    # https://github.com/MRK-YT/My-TelegramOrg-Bot/issues/3
+    # https://github.com/KazeDevID/MyTelegramOrg
     APP_DESCRIPTION = os.environ.get(
         "APP_DESCRIPTION",
         "created using https://telegram.dog/ItsMeKaze91"
