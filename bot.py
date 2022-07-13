c1fc7debf0a37f2c37132ade5548b151ca8c0499
Kaze <101078139+KazeDevID@users.noreply.github.com>
2022-07-13 08:12:57 +0800

    Update sample_config.py

--- bot.py
+++ bot.py
@@ -60,11 +60,11 @@ LOGGER = logging.getLogger(__name__)
 INPUT_PHONE_NUMBER, INPUT_TG_CODE = range(2)
 GLOBAL_USERS_DICTIONARY = {}
 
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "KenalSayaaa")
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ContactTim_bot")
 
 keyboard = [[
-            InlineKeyboardButton('My Developer', url=f"https://t.me/{OWNER_USERNAME}"),
-            InlineKeyboardButton('Source 👻', url="https://github.com/PunyaChael/MyTelegramOrg")
+            InlineKeyboardButton('Contact Owner', url=f"https://t.me/{OWNER_USERNAME}"),
+            InlineKeyboardButton('Bot 👻', url="https://t.me/GetIDAndHASH_bot")
         ]]
 
 def start(update, context):
